aa2c9ef256a5aa49b55f83bbd7125c558e033369
Akwill47 <wil.weat@hotmail.com>
2021-06-06 19:23:11 -0400

    comment

--- src/main/java/ex23/base/App.java
+++ src/main/java/ex23/base/App.java
@@ -1,22 +1,8 @@
 package ex23.base;
-import java.util.Scanner;
 /*
-Exercise 23 - Troubleshooting Car Issues
-An expert system is a type of artificial intelligence program that uses a knowledge base and a set of rules to perform a task that a human expert might do. Many websites are available that will help you self-diagnose a medical issue by answering a series of questions. And many hardware and software companies offer online troubleshooting tools to help people solve simple technical issues before calling a human.
-
-Create a program that walks the user through troubleshooting issues with a car. Use the following decision tree to build the system:
-Example Output
-Is the car silent when you turn the key? y
-Are the battery terminals corroded? n
-The battery cables may be damaged.
-Replace cables and try again.
-Constraint
-Ask only questions that are relevant to the situation and to the previous answers. Don’t ask for all inputs at once.
-
-Challenge
-Investigate rules engines and inference engines. These are powerful ways to solve complex problems that are based on rules and facts. Identify a rules engine for your programming language and use it to solve this problem
-*/
-
+ *  UCF COP3330 Summer 2021 Assignment 23 Solution
+ *  Copyright 2021 William Weatherlow
+ */
 public class App {
     public static void main(String[] args){
         SilentKey.step();
